2444bef513e65022d35f437dfed8ddce04fe1b24
LucazPlays <46164598+LucazPlays@users.noreply.github.com>
2024-11-01 17:07:53 +0100

    Update BetterBungeeAPI.java
    Updated the prefix and included a version number for HolyBungee

--- proxy/src/main/java/de/luca/betterbungee/BetterBungeeAPI.java
+++ proxy/src/main/java/de/luca/betterbungee/BetterBungeeAPI.java
@@ -8,7 +8,10 @@ public class BetterBungeeAPI {
     private static IPChecker ipchecker = new IPChecker();
 
     @Getter
-    private static String prefix = "§6BetterBungee";
+    private static String prefix = "§f§lHoly§6§lBungee";
+
+    @Getter
+    private static String HolyBungeeVersion = "1.0";
 
     @Getter
     private static String BetterBungeeVersion = "1.0";
